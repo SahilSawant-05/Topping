27bfcbc628fb1e70d31c8cfb613972408096a549
sahil <bhidebhai2828@gmail.com>
2025-12-23 13:11:14 +0530

    check

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-function test() {
-  alert("Widget working!");
-}
+// function test() {
+//   alert("Widget working!");
+// }
 
-ZOHO.embeddedApp.init();
+// ZOHO.embeddedApp.init();
+
+// ZOHO.embeddedApp.on("PageLoad", function (data) {
+//   test();
+// });
+
+ ZOHO.embeddedApp.on("PageLoad", function(data){
+      console.log(data);
+    });
+
+    function getContact(){
+      ZOHO.CRM.API.getRecord({
+        Entity: "Contacts",
+        RecordID: 123456789
+      }).then(console.log);
+    }
+
+    ZOHO.embeddedApp.init();
+
+
+
+ZOHO.CRM.API.insertRecord({
+  Entity: "Contacts",
+  APIData: {
+    First_Name: "John",
+    Last_Name: "Doe",
+    Email: "john@test.com"
+  }
+});
+ZOHO.CRM.CONNECTION.invoke("facebook_conn", {
+  url: "https://graph.facebook.com/v18.0/...",
+  method: "GET"
+});
